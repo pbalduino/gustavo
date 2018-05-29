09b774a12fee2bbb2eab77ef76c277d03a8471cb
Plínio Balduino <plinio.balduino@klarna.com>
2018-05-29 14:03:24 +0200

    First steps

---
+++ first.py
@@ -0,0 +1,11 @@
+# nome = input("Qual o seu nome? ")
+
+verso = "Meu nome é Mike gosto muito de brincar"
+
+# 3.141592
+
+print("")
+print(verso)
+print("")
+# ano = int(input("Em que ano você nasceu? "))
+# print("Olá,", nome, ". Você tem ou vai fazer", (2018 - ano), "anos esse ano.")
